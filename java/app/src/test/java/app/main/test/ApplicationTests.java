864f64f9cf26183d16f64dd1bcb69d92a773532a
Fanjasyiro <achmadfanjasyiro@gmail.com>
2023-10-25 23:44:23 +0700

    two commit

---
+++ java/app/src/test/java/app/main/test/ApplicationTests.java
@@ -0,0 +1,14 @@
+package app.main.test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+//@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApplicationTests {
+
+//	@Test
+	public void contextLoads() {
+	}
+
+}
